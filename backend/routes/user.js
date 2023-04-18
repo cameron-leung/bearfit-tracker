0c2f111c2f13e73a7c72e5496982d29c584f67f2
deepakragu <deepak.ragu@berkeley.edu>
2023-04-17 19:54:57 -0700

    Updated readme w/ phase 2 components (frontend) and renamed backend

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -3,7 +3,7 @@ const { check, validationResult } = require("express-validator");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const router = express.Router();
-const auth = require("./../middleware/auth");
+const auth = require("../middleware/auth");
 
 const User = require("../models/User");
 
